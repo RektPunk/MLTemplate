a9234743de63b3ef52596e0886c56912896d0275
RektPunk <rektpunk@gmail.com>
2022-09-05 00:37:54 +0900

    feat - add regression object

--- module/training/objective.py
+++ module/training/objective.py
@@ -4,7 +4,7 @@ import numpy as np
 import sklearn
 
 
-def optuna_objective(
+def optuna_classification_objective(
     trial,
     train_x: pd.DataFrame,
     train_y: pd.Series,
@@ -22,7 +22,7 @@ def optuna_objective(
         weight_train (pd.Series):
 
     Returns:
-        float: rmse score
+        float: log loss score
     """
     dtrain = lgb.Dataset(data=train_x, label=train_y)
     param = {
@@ -42,11 +42,50 @@ def optuna_objective(
     gbm = lgb.train(param, dtrain)
     preds = gbm.predict(valid_x)
 
-    ## binary classification
     pred_labels = np.rint(preds)
     log_loss = sklearn.metrics.log_loss(valid_y, pred_labels)
 
-    ## regression
-    # rmse = np.sqrt(np.mean((valid_y - preds) ** 2))
-
     return log_loss
+
+
+def optuna_regression_objective(
+    trial,
+    train_x: pd.DataFrame,
+    train_y: pd.Series,
+    valid_x: pd.DataFrame,
+    valid_y: pd.Series,
+) -> float:
+    """
+    optuna objective function
+    Args:
+        trial
+        train_x (pd.DataFrame):
+        train_y (pd.Series):
+        valid_x (pd.DataFrame):
+        valid_y (pd.Series):
+        weight_train (pd.Series):
+
+    Returns:
+        float: rmse
+    """
+    dtrain = lgb.Dataset(data=train_x, label=train_y)
+    param = {
+        "objective": "rmse",  # e.g. "rmse" for regression
+        "metric": "rmse",  # e.g. "rmse" for regression
+        "verbosity": -1,
+        "boosting_type": "gbdt",
+        "lambda_l1": trial.suggest_float("lambda_l1", 1e-8, 10.0, log=True),
+        "lambda_l2": trial.suggest_float("lambda_l2", 1e-8, 10.0, log=True),
+        "num_leaves": trial.suggest_int("num_leaves", 2, 256),
+        "feature_fraction": trial.suggest_float("feature_fraction", 0.4, 1.0),
+        "bagging_fraction": trial.suggest_float("bagging_fraction", 0.4, 1.0),
+        "bagging_freq": trial.suggest_int("bagging_freq", 1, 7),
+        "min_child_samples": trial.suggest_int("min_child_samples", 5, 100),
+    }
+
+    gbm = lgb.train(param, dtrain)
+    preds = gbm.predict(valid_x)
+
+    rmse = np.sqrt(np.mean((valid_y - preds) ** 2))
+
+    return rmse
